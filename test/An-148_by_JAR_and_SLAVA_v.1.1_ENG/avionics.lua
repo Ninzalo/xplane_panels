1c768e31791a04c829f351f017b34a5a18c08b3a
Vladislav <vlad.avdeev.88@mail.ru>
2023-08-10 19:58:19 +0300

    refactor: modify file avionics.lua

--- test/An-148_by_JAR_and_SLAVA_v.1.1_ENG/avionics.lua
+++ test/An-148_by_JAR_and_SLAVA_v.1.1_ENG/avionics.lua
@@ -3,3 +3,9 @@
 -- background image
 defineProperty("background", loadImage("whitebackground.png", 0, 0, 1920, 1200 ))
 
+components = {
+    texture { 
+        position = { 0, 0, 1920, 1200 },
+        image = get(background)
+    },
+}
