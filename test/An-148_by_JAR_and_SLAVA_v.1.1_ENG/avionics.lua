559271bb80263f9628e6185ae6d99d3bb8a8fb6e
Vladislav <vlad.avdeev.88@mail.ru>
2023-08-10 20:12:33 +0300

    refactor: create new file and modify file avionics.lua

--- test/An-148_by_JAR_and_SLAVA_v.1.1_ENG/avionics.lua
+++ test/An-148_by_JAR_and_SLAVA_v.1.1_ENG/avionics.lua
@@ -1,7 +1,7 @@
 ﻿size = {2048, 2048}
 
 -- background image
-defineProperty("background", loadImage("whitebackground.png", 0, 0, 1920, 1200 ))
+defineProperty("background", loadImage("blackbackground.png", 0, 0, 1920, 1200 ))
 
 components = {
     texture { 
