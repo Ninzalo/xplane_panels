ca5fe651381543a2d233e121099f7119e21e8f9c
Vladislav <vlad.avdeev.88@mail.ru>
2023-08-15 18:26:40 +0300

    refactor: edit capt_panel.lua

--- test/An-148_by_JAR_and_SLAVA_v.1.1_ENG/Custom Avionics/capt_panel.lua
+++ test/An-148_by_JAR_and_SLAVA_v.1.1_ENG/Custom Avionics/capt_panel.lua
@@ -1,19 +1,11 @@
 size = {1920, 1200}
+
 -----------------
 -- VARIABLES
 -----------------
-defineProperty("capt_subpanel", globalPropertyi("sim/an148/capt_subpanel"))
--- defineProperty("gpu_on_power", globalPropertyf("sim/cockpit/electrical/gpu_on"))
--- defineProperty("inverter_on_1", globalPropertyi("sim/cockpit2/electrical/inverter_on[0]"))
--- defineProperty("inverter_on_2", globalPropertyi("sim/cockpit2/electrical/inverter_on[1]"))
--- defineProperty("inverter_on_3", globalPropertyi("sim/an148/inverter_on_3"))
--- defineProperty("generator_on_1", globalPropertyi("sim/cockpit2/electrical/generator_on[0]"))
--- defineProperty("generator_on_2", globalPropertyi("sim/cockpit2/electrical/generator_on[1]"))
--- defineProperty("generator_on_apu", globalPropertyi("sim/cockpit2/electrical/APU_generator_on"))
--- defineProperty("battery_on_1", globalPropertyi("sim/cockpit2/electrical/battery_on[0]"))
--- defineProperty("battery_on_2", globalPropertyi("sim/cockpit2/electrical/battery_on[1]"))
--- defineProperty("battery_on_3", globalPropertyi("sim/cockpit2/electrical/battery_on[2]"))
 
+-- ПАНЕЛЬ КВС
+defineProperty("capt_subpanel", globalPropertyi("sim/an148/capt_subpanel"))
 -- Самолёт под током
 defineProperty("dc_bus", globalPropertyi("sim/an148/dc_bus"))
 -- Температура первого и второго двигателей (сектор)
@@ -25,32 +17,23 @@ defineProperty("ENGN_EGT_c2", globalPropertyf("sim/flightmodel/engine/ENGN_EGT_c
 -- Давление масла первого и второго двигателей (сектор)
 defineProperty("oil_pressure1", globalPropertyf("sim/cockpit2/engine/indicators/oil_pressure_psi[0]"))
 defineProperty("oil_pressure2", globalPropertyf("sim/cockpit2/engine/indicators/oil_pressure_psi[1]"))
+
+defineProperty("ENGN_N1_1", globalPropertyf("sim/flightmodel/engine/ENGN_N1_[0]"))
 -----------------
 local mfi_mnemo_eng = 0
--- local mfi_mnemo_fuel = 0
--- local mfi_mnemo_conf = 0
--- local mfi_mnemo_air = 0
--- local mfi_mnemo_elec = 0
--- local mfi_mnemo_hydr = 0
--- local mfi_mnemo_sys = 1
--- local mfi_mnemo_stat = 0
--- local mfi_mnemo_tcas = 0
+
 -----------------
 -- IMAGES
 -----------------
+
 defineProperty("background", loadImage("capt_panel_new.png"))
 defineProperty("rotary_small", loadImage("rotary_small.png"))
 defineProperty("yellow_light", loadImage("yellow_light.png"))
 defineProperty("green_sector", loadImage("green_sector.png"))
 defineProperty("eng_mnemo", loadImage("eng_mnemo.png"))
 defineProperty("digitsImage", loadImage("white_digit_strip.png", 0, 0, 16, 196))
--- local switch_sound = loadSample('Custom Sounds/metal_switch.wav')
--- local switch_plastic = loadSample('Custom Sounds/plastic_switch.wav')
+
 local switch_push = false
--- local vor1_btn = 1
--- local vor2_btn = 1
--- local adf1_btn = 0
--- local adf2_btn = 0
 
 local function map(x, in_min, in_max, out_min, out_max)
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
@@ -72,9 +55,11 @@ local function coords_converter(x_old, y_old, width, height)
     height
   )
 end
+
+---- КОМПОНЕНТЫ
 components = {
-------------------------------------------------------------------------------
---background
+
+-- Фон
 texture{ 
 	image = get(background),
 	position = {0, 0, size[1], size[2]},
@@ -217,6 +202,21 @@ digitstape {
     return get(mfi_mnemo_eng) == 1 and get(dc_bus) == 1
   end,
 },
+
+-- eng1_n1 digits
+digitstape {
+    position = { 1447, 635, 35, 15},
+    image = digitsImage,
+    digits = 3,
+    showLeadingZeros = false,
+    allowNonRound = true,
+    value = function()
+  return get(ENGN_N1_1)
+end,
+valueEnabler = function()
+  return get(mfi_mnemo_eng) == 1 and get(dc_bus) == 1
+end,
+};
 
 ---- Переключение видимости дисплея двигателей  
 switch {
